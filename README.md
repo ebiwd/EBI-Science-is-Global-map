0d94dbd5b55530265454df7241ef53da04635e56
khawkins98 <khawkins98@gmail.com>
2017-03-20 16:50:21 +0000

    Initial demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # EMBL-EBI-Science-is-Global-map
-This shows where staff have come from, and where they've gone -- from March 2017 onwards..
+This shows where staff have come from, and where they've gone -- from March 2017 onwards.
 
-[View the map](sample-embed.html)
+[View the map](https://ebiwd.github.io/EBI-Science-is-Global-map/sample-embed.html)
 
 For usage guidance, view the source of [ebi-science-is-global-map.html](ebi-science-is-global-map.html)
--- assets/map_scripts.js
+++ assets/map_scripts.js
@@ -1,25 +1,19 @@
 // handy things:
 // https://www.mapbox.com/mapbox.js/example/v1.0.0/markercluster-multiple-groups/
 // https://github.com/Leaflet/Leaflet.markercluster#usage
-// http://wwwdev.ebi.ac.uk/ebiwebtrafficmap/kmlvector.html
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2hhd2tpbnNlYmkiLCJhIjoiY2ludTZ2M3ltMDBtNXczbTJueW85ZmJjNyJ9.u6SIfnrYvGe6WFP3fOtaVQ';
 
-var map = L.mapbox.map('map');
+var geocoder = L.mapbox.geocoder('mapbox.places'),
+    map = L.mapbox.map('map');
 
 // show sunrise and sunset
 // http://joergdietrich.github.io/Leaflet.Terminator/
-var daynightoverlay = L.terminator({className:'daynightoverlay'});
-map.setView([30, daynightoverlay.getLatLngs()[0][700]['lng']], 2);
-// map.setView([30, 10], 3);
-daynightoverlay.addTo(map);
+// map.setView([30, daynightoverlay.getLatLngs()[0][700]['lng']], 2);
+map.setView([30, 10], 3);
+// daynightoverlay.addTo(map);
 
-setInterval(function(){updateTerminator(daynightoverlay)}, 2000);
-function updateTerminator(t) {
-  var t2 = L.terminator();
-  t.setLatLngs(t2.getLatLngs());
-  t.redraw();
-}
+// setInterval(function(){updateTerminator(daynightoverlay)}, 2000);
 
 L.tileLayer(
   'https://api.mapbox.com/styles/v1/khawkinsebi/cio2mav7q0018c2nk2vvg8xgt/tiles/{z}/{x}/{y}?access_token=' + L.mapbox.accessToken, {
@@ -72,7 +66,7 @@ function newMarkerClusterGroup(clusterColors,targetClusterCSSClass,clusterPopUpH
 
   clusterName.on('clustermouseover', function(ev) {
     // Child markers for this cluster are a.layer.getAllChildMarkers()
-    L.popup().setLatLng(ev.latlng).setContent(clusterPopUpHTML).openOn(map); 
+    L.popup().setLatLng(ev.latlng).setContent(clusterPopUpHTML).openOn(map);
   });
   clusterName.on('clustermouseout', function(ev) {
     map.closePopup();
@@ -81,130 +75,23 @@ function newMarkerClusterGroup(clusterColors,targetClusterCSSClass,clusterPopUpH
   return clusterName;
 }
 
-function processData(fetchedData) {
-  var markerClustersTemporary = [];
-  
-  fetchedData.eachLayer( function(marker) {
-    if (typeof marker.feature !== 'undefined') {            
-      // add to marker group
-      var a = marker.feature.geometry.coordinates;
-      // var title = 'tesdt';
-      var markerNode = L.circleMarker(new L.LatLng(a[1], a[0]), { 
-        stroke: true,
-        color: 'rgb(202,210,211)',
-        weight: '1',
-        // fillColor: targetClusterGroupColor,
-        fillOpacity: '.4'
-      });
-      markerNode.setRadius(5);
-      // markerNode.bindPopup(title);
-      // group the layers by data source
-      var targetLayer = marker.feature.properties['target-layer'];
-      if (markerClustersTemporary[targetLayer] == undefined) { markerClustersTemporary[targetLayer] = []; }
-      markerClustersTemporary[targetLayer].push(markerNode);
-    }
-  });
-
-  // process new nodes
-  for(var index in markerClustersTemporary) { 
-    removeAndAddNodes(index,markerClustersTemporary[index]);  
-  }
-
-}
-
 // Progressivley load and unload nodes based on the number of nodes to move, and how quickly we pull data.
 // We do this so the maps is less chunky adding and removing data.
-function removeAndAddNodes(targetClusterGroup,queueToAdd) {
+function addNodes(targetClusterGroup,queueToAdd) {
 
   targetClusterGroup = window[targetClusterGroup]; // reattach passed string to mapbox layer
 
-  function addNodes() {
-    if (mainLoopPause === true) return;
+  var nodeToProcess = queueToAdd.pop();
 
-    if (queueToAdd.length == 0) {
-      clearInterval(addNodesVar); // we're done
-    } else {
-      var nodeToProcess = queueToAdd.pop(); 
-
-      if ((nodeToProcess._latlng.lat == '0') && (nodeToProcess._latlng.lng == '0')) {
-        return false; // exit if the location didn't geocode
-      }
-
-      // add dot to map
-      targetClusterGroup.addLayer(nodeToProcess);
-      // Performance note:
-      // If things get sluggish, we should consider using the plural function "addLayers" 
-      // view-source:http://leaflet.github.io/Leaflet.markercluster/example/marker-clustering-realworld.50000.html
-      // We could also group the source CSV to say how many occurances of each lat/long there is
-
-      // schedule marker removal
-      setTimeout(function() { 
-        scheduledNodeRemoval(targetClusterGroup,nodeToProcess);
-      }, lifeSpan * 2); 
-    }
+  if ((nodeToProcess._latlng.lat == '0') && (nodeToProcess._latlng.lng == '0')) {
+    return false; // exit if the location didn't geocode
   }
 
-  // run the queue
-  var addNodesVar = window.setInterval(addNodes, lifeSpan / queueToAdd.length+1);
+  // add dot to map
+  targetClusterGroup.addLayer(nodeToProcess);
 }
 
-function scheduledNodeRemoval(targetClusterGroup,targetNode) {
-  if (mainLoopPause === false) {
-    targetClusterGroup.removeLayer(targetNode);
-  } else {
-    // wait XXms and check again
-    setTimeout(function() { 
-      scheduledNodeRemoval(targetClusterGroup,targetNode);
-    }, lifeSpan); 
-  }
-}
 
-// loop to keep clusters updating
-function runClusters() {
-
-  if (mainLoopPause === true) return;
-
-  // we just ask the server and it will return all desired data sets
-  $('.data-freshness').fadeTo( "fast", 0.5 );
-
-  $.ajax({
-    type: "GET",
-    url: 'parseLogMap.php?file=all&cachebust='+Math.floor((Math.random() * 1000) + 1),
-    dataType: "text",
-    success: function(data, textStatus, request) { 
-      // compare the cache file time to request server time and if it's out of step, take steps to align with "Fresh" server time
-      var offset = 0;
-      if (request.getResponseHeader('Map-cache-creation') > 0) {
-        offset = request.getResponseHeader('Map-cache-server-time') - request.getResponseHeader('Map-cache-creation');
-      }
-      if (offset === 0) {
-        // tell user map is fresh, but wait for old dots to sync up first 
-        setTimeout(function() { 
-          $('.leaflet-control-attribution .data-freshness').html('Map is fresh ');       
-        }, lifeSpan - 500 );
-      } else {
-        // inch closer to actual time
-        window.clearInterval(mainLoop);
-        mainLoopPause = true;
-        setTimeout(function() { 
-          mainLoop = window.setInterval(runClusters, lifeSpan); // schedule future updates  
-          mainLoopPause = false;
-        }, 500 );
-
-        $('.leaflet-control-attribution .data-freshness').html('Data is ' + offset + ' seconds stale ... compensating. ');       
-
-      }
-      parseDate(data,offset);
-    }
-  });
-
-  function parseDate(data) {
-    var readyData = omnivore.csv.parse(data, null);
-    processData(readyData);
-    $('.data-freshness').fadeTo( "fast", 1 );
-  }
-
-}
 
 // setup colours and markercluster objects
 var counter = 0;
@@ -220,60 +107,81 @@ map.addLayer(markerClustersEBI);
 map.addLayer(markerClustersPortals);
 map.addLayer(markerClustersUniprot);
 
-var lifeSpan = 6000; // how quickly we fetch data, and how long each dot lasts
- // NOTE: this needs to match up with the server side cach frequency and needs to be faster
-var mainLoopPause = false; // functionality for a "pause button"
-var mainLoop = window.setInterval(runClusters, lifeSpan); // schedule futur updates  
+function markerOptions(layer) {
+  return {opacity: 0, fillOpacity: 1, radius: 3, color: '#000'};
+}
 
-// add a holder for data freshness
-$('.leaflet-control-attribution').prepend('<span class="data-freshness"></span> ');
+// pull the data
+$.ajax({
+  type: "GET",
+  url: 'assets/data.csv',
+  dataType: "text",
+  success: function(data, textStatus, request) {
+    // data = data.replace(/(?:\r\n|\r|\n)/g, '; ');
+    // console.log(data);
+    data = csvJSON(data);
 
-// run the data pull immediately on strap 
-runClusters(); 
+    // cleanup
+    data = data.replace(/"\\"/g, '"'); // double quote at start
+    data = data.replace(/\\""/g, '"'); // double quote at end
+    data = data.replace(/nope;/g, '');
+    data = data.replace(/No idea;/g, '');
 
-// Pause toggling
-$('a.pause').on('click',function(){
-  if (mainLoopPause === false) {
-    mainLoopPause = true;
-    $('a.pause').html('<span class="icon icon-functional" data-icon="v"></span>Play');
-  } else {
-    mainLoopPause = false;
-    $('a.pause').html('<span class="icon icon-functional" data-icon="o"></span>Pause');
+    // console.log(data);
+    data = JSON.parse(data)[0];
+    // console.log(data);
+
+    var parsedData = ['born','unniversity','first_job','work_previous','work_now','work_future'];
+
+    $.each(data, function(index, value) {
+      console.log(index,dataRow);
+      var dataRow = value.split(';');
+      for (var i = 0; i < dataRow.length; i++) {
+        console.log(dataRow[i]);
+        geocoder.query(dataRow[i], addPoint);
+      }
+    });
+
+    // geocoder.query('Chester, NJ', addPoint);
+
+    function addPoint(err, geoResponse) {
+      // The geocoder can return an area, like a city, or a
+      // point, like an address. Here we handle both cases,
+      // by fitting the map bounds to an area or zooming to a point.
+      console.log(geoResponse);
+      // http://leafletjs.com/reference.html#path-options
+      L.circleMarker([geoResponse.latlng[0], geoResponse.latlng[1]],markerOptions('tocome')).addTo(map);
+      if (geoResponse.lbounds) {
+        // L.marker([-45, -45]).addTo(map);
+        // map.fitBounds(geoResponse.lbounds);
+      } else if (geoResponse.latlng) {
+        // map.setView([geoResponse.latlng[0], geoResponse.latlng[1]], 13);
+      }
+    }
+
+    // var markerClustersTemporary = [];
+    //
+    // // add to marker group
+    // var a = marker.feature.geometry.coordinates;
+    // // var title = 'tesdt';
+    // var markerNode = L.circleMarker(new L.LatLng(a[1], a[0]), {
+    //   stroke: true,
+    //   color: 'rgb(202,210,211)',
+    //   weight: '1',
+    //   // fillColor: targetClusterGroupColor,
+    //   fillOpacity: '.4'
+    // });
+    // markerNode.setRadius(5);
+    // // markerNode.bindPopup(title);
+    // // group the layers by data source
+    // var targetLayer = marker.feature.properties['target-layer'];
+    // if (markerClustersTemporary[targetLayer] == undefined) { markerClustersTemporary[targetLayer] = []; }
+    // markerClustersTemporary[targetLayer].push(markerNode);
+
+    // process new nodes
+    // for(var index in markerClustersTemporary) {
+    //   addNodes(index,markerClustersTemporary[index]);
+    // }
+
   }
 });
-
-// Bonus: on IE10+ (and other modern browsers), pause the map when the tab doesn't show
-// https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
-
-// Set the name of the hidden property and the change event for visibility
-var hidden, visibilityChange; 
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
-  hidden = "hidden";
-  visibilityChange = "visibilitychange";
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden";
-  visibilityChange = "msvisibilitychange";
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden";
-  visibilityChange = "webkitvisibilitychange";
-}
- 
-function handleVisibilityChange() {
-  if (document[hidden]) {
-    // only pause if the tab is playing
-    if (mainLoopPause === false) { $('a.pause').click(); }
-    // $('#pause-message').remove();
-    // $('body').append('<div id="pause-message" class="message modal">Noticed you switched away, we\'ll resume the map</div>');
-  } else {
-    $('a.pause').click();
-    // $('#pause-message').hide(1500);
-  }
-}
-
-// Warn if the browser doesn't support addEventListener or the Page Visibility API
-if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
-  // console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
-} else {
-  // Handle page visibility change   
-  document.addEventListener(visibilityChange, handleVisibilityChange, false);
-}
